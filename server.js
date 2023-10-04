611e1d9fcb565da231cf47b6577fc232f27d2b4c
Anandan Balasupramanian <anandan.bs@gmail.com>
2023-10-04 21:51:00 +0530

    mongo and  ui build with domain name change

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ const storage = multer.diskStorage({
 app.use(express.static(filepath.join(__dirname, 'build')));
 
 app.get('/', function (req, res) {
-  res.sendFile(filepath.join(__dirname, 'build', 'index.html'));
+    res.sendFile(filepath.join(__dirname, 'build', 'index.html'));
 });
 
 // const upload = multer({ storage: storage });
@@ -55,21 +55,54 @@ app.post('/upload', multer({ storage: getMutlerConfig() }).array('files', 200), 
         return res.status(400).send('No files were uploaded.' + JSON.stringify(req.body));
     }
     const folder = `uploads/${slugify(req.body.date.toString())}/${slugify(req.body.entity.toString())}/`;
-    fs.appendFile('uploads/files.json', JSON.stringify({ ...req.body, files: req.files, folder, timestamp: Date.now() }), function (err) {
+    const photoInfo = { ...req.body, files: req.files, folder, timestamp: Date.now() };
+    fs.appendFile('uploads/files.json', JSON.stringify(data), function (err) {
         if (err) throw err;
         console.log('Saved!');
     });
 
-
+    updateDocument(photoInfo);
     res.send('Files uploaded successfully at ' + folder);
-
 });
-
-
-
 
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
+const { MongoClient, ObjectID } = require('mongodb');
+
+// Connection URI for your MongoDB server
+const uri = 'mongodb://localhost:27017/photos'; // Replace with your database URI
+
+
+// Function to update the document
+async function updateDocument(updateData) {
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    try {
+        // Connect to MongoDB
+        await client.connect();
+
+        const collection = client.db().collection('your-collection-name'); // Replace with your collection name
+
+        // Define the filter to find the document you want to update
+        const filter = { email_id: updateData.email_id };
+
+        // Define the update operation
+        const updateOperation = {
+            $set: updateData // Use $set to update the specified fields
+        };
+
+        // Perform the update
+        const result = await collection.updateOne(filter, updateOperation);
+
+        console.log(`Document updated: ${result.modifiedCount} document(s) modified`);
+    } finally {
+        // Close the MongoDB connection
+        await client.close();
+    }
+}
+
+// Call the updateDocument function to update the document
+updateDocument().catch(console.error);
