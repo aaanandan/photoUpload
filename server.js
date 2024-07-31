97d129ea426726c5669bcc5c4dfb97abb3b4b4be
Anandan Balasupramanian <anandan.bs@gmail.com>
2024-07-31 20:13:23 +0530

    uncommented event page creation

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.post('/upload', multer({ storage: getMutlerConfig() }).array('files', 200), 
         const result = await client.db("photos").collection("photos").insertOne(photoInfo);
         await updateSheet(photoInfo);
         //TODO: Fix activityType files to support map function to enable wikik event page creation 
-        //await createWikiEventPage(photoInfo);
+        await createWikiEventPage(photoInfo);
         console.log(`New record added: ${result.insertedId}`);
     }
     return res.status(200).send({ 'message': 'files uploaded sucessfully to :' + folder });
